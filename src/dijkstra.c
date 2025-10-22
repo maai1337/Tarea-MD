ccbd9cd569d3971d40fcc74372b30cf2bc9cb14f
SebastianNovaOne <sebaxnovax@gmail.com>
2025-10-21 22:21:55 -0300

    Cambios menores a dijkstra.c para recibir cantidad de vértices y matriz de adyacencia directamente desde graph.c

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "graph.h"
 
 #define INF 999
 
@@ -10,26 +11,25 @@ typedef struct {
     int padre;
 } Vertice;
 
-
 void inicializar_vertice(Vertice *v) {
     v->distancia = INF;
     v->visto = false;
-    v->padre = -1;  
+    v->padre = -1;
 }
 
 bool hayNoVisitado(Vertice vertices[], int num_vertices) {
     for (int i = 0; i < num_vertices; i++) {
-        if (!vertices[i].visto) { 
+        if (!vertices[i].visto) {
             return true;
         }
     }
-    return false;  
+    return false;
 }
 
 int seleccionarMinimo(Vertice vertices[], int num_vertices) {
-    int minDist = INF;   
+    int minDist = INF;
     int minIndex = -1;
-    
+
     for (int i = 0; i < num_vertices; i++) {
         if (!vertices[i].visto && vertices[i].distancia < minDist) {
             minDist = vertices[i].distancia;
@@ -40,13 +40,8 @@ int seleccionarMinimo(Vertice vertices[], int num_vertices) {
     return minIndex;
 }
 
-char indice_a_letra(int indice) {return 'A' + indice;}
-
-//main 1
-void imprimir_camino(int num_vertices, Vertice vertices[num_vertices], int final){
-
+void imprimir_camino(int num_vertices, Vertice vertices[num_vertices], int final) {
     int indice = final;
-
 
     if (vertices[indice].distancia == INF) {
         printf("La distancia entre los vertices ingresados es infinito");
@@ -57,60 +52,54 @@ void imprimir_camino(int num_vertices, Vertice vertices[num_vertices], int final
     int contador = 0;
 
     while (indice != -1) {
-        camino[contador] = indice_a_letra(indice); 
+        camino[contador] = indice_a_letra(indice);
         contador++;
-        indice = vertices[indice].padre; 
+        indice = vertices[indice].padre;
     }
 
     printf("El camino más corto es: ");
 
-    for (int i = contador - 1; i >=0; i--){
+    for (int i = contador - 1; i >= 0; i--) {
         printf("%c", camino[i]);
-        if (i>0) {
+        if (i > 0) {
             printf(" ");
         }
-    }   
+    }
 }
 
-//main 2 
-void Dijkstra(int num_vertices, int matriz_adyacencia[num_vertices][num_vertices], int inicio, int final){
-    
-    Vertice vertices[num_vertices];
+char indice_a_letra(int indice) {
+    return 'A' + indice;
+}
 
-    for(int i = 0; i < num_vertices; i++) {
+void Dijkstra(Grafo* g, int inicio, int final) {
+    Vertice vertices[(*g).numVertices];
+
+    for (int i = 0; i < (*g).numVertices; i++) {
         inicializar_vertice(&vertices[i]);
     }
-    
-    vertices[inicio].distancia = 0; 
 
-    while (hayNoVisitado(vertices, num_vertices)) {
-        int min_index = seleccionarMinimo(vertices, num_vertices);
+    vertices[inicio].distancia = 0;
+
+    while (hayNoVisitado(vertices, (*g).numVertices)) {
+        int min_index = seleccionarMinimo(vertices, (*g).numVertices);
         if (min_index == -1) { // Todos los nodos son inalcanzables
             break;
         }
 
         vertices[min_index].visto = true;
 
-        for (int i = 0; i < num_vertices; i++) {                            // Para todo
-            if (!vertices[i].visto && matriz_adyacencia[min_index][i] > 0){ // vecino de u
-
-                int peso_uv = matriz_adyacencia[min_index][i];
+        for (int i = 0; i < (*g).numVertices; i++) {
+            // Para todo vecino de u
+            if (!vertices[i].visto && (*g).matrizAdyacencia[min_index][i] > 0) {
+                int peso_uv = (*g).matrizAdyacencia[min_index][i];
 
                 if (vertices[i].distancia > vertices[min_index].distancia + peso_uv) {
                     vertices[i].distancia = vertices[min_index].distancia + peso_uv;
                     vertices[i].padre = min_index;
                 }
-
             }
-            
         }
-        
     }
 
-
-    imprimir_camino(num_vertices, vertices, final);
-    
+    imprimir_camino((*g).numVertices, vertices, final);
 }
-
-
-
