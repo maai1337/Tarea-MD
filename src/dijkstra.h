cb7d69dcee709d644b5211c24e2e8ee2fbf661e5
Catalinafer2023 <fernanda.catalina.rubilar@gmail.com>
2025-10-21 22:27:54 -0300

    dijkstra.h terminado

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -11,5 +11,9 @@ typedef struct {
 
 void dijkstra(int num_vertices, int** matriz_adyacencia, int inicio, int final);
 void imprimir_camino(int num_vertices, Vertice vertices[], int final);
+char indice_a_letra(int indice);
+int seleccionarMinimo(Vertice vertices[], int num_vertices);
+bool hayNoVisitado(Vertice vertices[], int num_vertices);
+void inicializar_vertice(Vertice *v);
 
 #endif
